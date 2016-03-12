c830b7c023143c58c334ae17a1e9a96cd11eacad
Alvaro Viebrantz <alvarowolfx@gmail.com>
2016-03-11 23:11:46 -0400

    Add map and geolocation request

--- src/ShakeIt.js
+++ src/ShakeIt.js
@@ -15,26 +15,57 @@ import React, {
     TouchableHighlight
 } from 'react-native';
 
+import MapView from 'react-native-maps';
+
 export default class ShakeIt extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
+
+        this.state = {
+            region: null
+        };
+
         this._onPress = this._onPress.bind(this);
+        this._onLocationFound = this._onLocationFound.bind(this);
+        this._requestLocation = this._requestLocation.bind(this);
     }
 
-    _onPress(){
-        console.log('Olá console');
-        alert('Olá novamente');
+    _onPress() {
+        this._requestLocation();
+    }
+
+    _requestLocation(){
+        navigator.geolocation.getCurrentPosition(this._onLocationFound,
+            (error) => alert(error.message),
+            {timeout: 10000, enableHighAccuracy: true});
+    }
+
+    _onLocationFound(position){
+        let region = position.coords;
+        region.latitudeDelta = 0.01;
+        region.longitudeDelta = 0.01;
+        this.setState({
+            region
+        })
+    }
+
+    componentDidMount(){
+        this._requestLocation();
     }
 
     render() {
         return (
             <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    Olá Dev MT
-                </Text>
-                <TouchableHighlight style={styles.button} onPress={this._onPress}>
-                    <Text style={styles.buttonText}>Touch me</Text>
-                </TouchableHighlight>
+                <View style={styles.messageContainer}>
+                    <Text style={styles.welcome}>
+                        Olá Dev MT
+                    </Text>
+                    <TouchableHighlight style={styles.button} onPress={this._onPress}>
+                        <Text style={styles.buttonText}>Center on me</Text>
+                    </TouchableHighlight>
+                </View>
+                <MapView style={styles.mapContainer} region={this.state.region}
+                         showsUserLocation={true}/>
             </View>
         );
     }
@@ -42,10 +73,16 @@ export default class ShakeIt extends Component {
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1
+    },
+    messageContainer: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#F5FCFF',
+    },
+    mapContainer: {
+        flex: 3
     },
     welcome: {
         fontSize: 20,
